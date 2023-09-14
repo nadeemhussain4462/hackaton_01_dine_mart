b96f798b2b62a3f91ba305472415ff8806992cfe
Nadeem Hussain <81299514+nadeemhussain4462@users.noreply.github.com>
2023-09-14 19:06:11 +0500

    update

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ const getProductsDetail = (id: number | string) => {
   return Products.filter((product) => product.id == id);
 };
 
+const sizes = ["xs", "sm", "md", "lg", "xl"];
 export default function Page({ params }: { params: { id: string } }) {
   const result = getProductsDetail(params.id);
   return (
@@ -19,14 +20,30 @@ export default function Page({ params }: { params: { id: string } }) {
           <div>
             <div>
               <h1 className="text-2xl">{product.name}</h1>
-              <h2 className="text-base font-semibold text-gray-400">{product.tagline}</h2>
+              <h2 className="text-base font-semibold text-gray-400">
+                {product.tagline}
+              </h2>
             </div>
             <div>
-              <h3 className="mt-6 text-xs font-semibold">SELECT SIZE</h3>
-              <div></div>
+              <h3 className="mt-6 text-[10px] font-semibold">SELECT SIZE</h3>
+              {/* {sizes} */}
+              <div className="flex gap-x-3 mt-3">
+                {sizes.map((item) => {
+                  return (
+                    <div className="flex justify-center items-center rounded-full border hover:shadow-xl h-7 w-7">
+                      <span className="text-[12px] text-center font-semibold text-gray-500">
+                        {item}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+              {/* {Quantity} */}
+              <div className="flex mt-6">
+                <h3 className="text-[12px] font-semibold self-center">Quantity :</h3>
+                <div>1</div>
+              </div>
             </div>
-            <p>Price {product.price}</p>
-            <p>Category {product.category}</p>
           </div>
         </div>
       ))}
